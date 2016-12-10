e893be96ac261b705d78bc50246fc88bf96f083b
zhuxiaona <1217218718@qq.com>
2016-12-10 16:03:17 +0800

    11

---
+++ demo/js/c_index.js
@@ -0,0 +1,14 @@
+//window.setInterval('showTime()',100)
+//function showTime(){
+//	var enabled = 0; 
+//	today = new Date();
+//	var day; 
+//	var date;
+//	date = today.getHours()+":"+today.getMinutes()
+//	//+":"+today.getSeconds();
+//	$("#time").html(date);
+//	showTime()
+//};
+$(document).ready(function(){
+
+});
